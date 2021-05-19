1402c855e9c10b818cca6e9ada03613d50b07960
Bestbrain Igbaji Livinus <bestbrain10@gmail.com>
2021-05-19 15:48:36 +0100

    turn off logger in production

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: process.env.NODE_ENV === 'production',
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
