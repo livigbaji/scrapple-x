b70e5ed2bd6636b1cf05b480e672024eb594459e
Bestbrain Igbaji Livinus <bestbrain10@gmail.com>
2021-05-19 20:01:46 +0100

    add a no-sandbox option

--- src/crawler.service.ts
+++ src/crawler.service.ts
@@ -50,7 +50,9 @@ export class CrawlerService {
     }
 
     try {
-      const browser = await puppeteer.launch({});
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
       const page = await browser.newPage();
       await page.goto(url);
 
